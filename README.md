68300e6fe0716c6bc0d0faf70e77dfa11b46f1bf
Sarina Chang <sarinachang7@gmail.com>
2022-09-26 19:00:44 -0400

    file cleanup / css setup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Interactive Card
 
-Frontend Mentor challenge.
+Frontend Mentor <a href="https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw">challenge</a>.
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import '../css/App.css';
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <h1>kill me plss</h1>
+    </div>
+  );
 }
 
 export default App;
