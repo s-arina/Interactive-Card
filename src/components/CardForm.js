cda13f8bda350c6dbba52152a3c69d616cc2b716
Sarina Chang <sarinachang7@gmail.com>
2022-09-27 01:45:59 -0400

    form css

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import '../css/CardForm.css';
 
 function CardForm(props) {
   return (
     <div className='card-form'>
-      <h1>card form</h1>
+      <form id='card-info'>
+        <label htmlFor='cardholder-name'>CARDHOLDER NAME</label>
+        <input
+          type='text'
+          id='cardholder-name'
+          placeholder='e.g. Jane Appleseed'
+          required
+        />
+        <label htmlFor='card-number'>CARD NUMBER</label>
+        <input
+          type='text'
+          id='card-number'
+          placeholder='e.g. 1234 5678 9123 0000'
+          required
+        />
+        <div className='date-cvc'>
+          <div className='exp-date'>
+            <label htmlFor='date'>EXP. DATE (MM/YY)</label>
+            <div className='date'>
+              <input type='number' id='month' placeholder='MM' required />
+              <input type='number' id='year' placeholder='YY' required />
+            </div>
+          </div>
+          <div className='cvc'>
+            <label htmlFor='cvc'>CVC</label>
+            <input type='number' id='cvc' placeholder='e.g. 123' required />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
